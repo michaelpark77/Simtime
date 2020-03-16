fd631baef7f2c2eaaf48c77ea17154f6ea74c83f
arara90 <ararajo@gmail.com>
2020-03-16 14:29:05 +0900

    modal init

---
+++ Simtime/frontend/src/components/layout/Modal.js
@@ -0,0 +1,20 @@
+// Modal
+const modalRoot = document.getElementById("modal-root");
+class Modal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.el = document.createElement("div");
+  }
+
+  componentDidMount() {
+    modalRoot.appendChild(this.el);
+  }
+
+  componentWillUnmount() {
+    modalRoot.removeChild(this.el);
+  }
+
+  render() {
+    return ReactDOM.createPortal(this.props.children, this.el);
+  }
+}
