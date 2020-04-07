2a46132e39e2f46142ad447eb0f13c22d64d7664
arara90 <ararajo@gmail.com>
2020-04-07 21:25:16 +0900

    colors

--- Simtime/frontend/src/AtomicComponents/B-Molecules/header.js
+++ Simtime/frontend/src/AtomicComponents/B-Molecules/header.js
@@ -1,3 +1,5 @@
+import { ST_YELLOW, ST_BLUE, TEXT_ACTIVE, TEXT_INACTIVE } from "../Colors";
+
 import React from "react";
 import styled, { css } from "styled-components";
 import PropTypes from "prop-types";
---
+++ Simtime/frontend/src/AtomicComponents/Colors.js
@@ -0,0 +1,32 @@
+// background : _LIGHT, (NONE) , _DARK
+// text : active, inactive, link, Visited
+
+// MAIN COLORS
+export const ST_YELLOW_LIGHT = "#FFF8E7";
+export const ST_YELLOW = "#F2C94C"; //main (Deep) yellow
+
+export const ST_PINK = "#E7156E";
+export const ST_BLUE = "#F2C94C";
+export const ST_ORANGE = "#F96A32";
+export const ST_RED = "#E90116";
+export const ST_VIOLET = "#69366E";
+
+export const ST_GREEN = "#028538";
+export const ST_GREEN_NEON = "#0081C2";
+
+export const ST_GRAY_LIGHT = "#FBFBFB";
+export const ST_GRAY = "#D0C9D6";
+export const ST_GRAY_DARK = "#F4F3F1";
+
+export const ST_WHITE = "#FFFFFF";
+export const ST_BLACK = "#2B2738";
+
+// TEXT
+export const TEXT = ST_BLACK;
+export const TEXT_LINK = ST_BLUE;
+export const TEXT_VISITED = ST_VIOLET;
+export const TEXT_ACTIVE = ST_TEXT;
+export const TEXT_INACTIVE = ST_GRAY;
+
+//ETC
+export const CHECK_GREEN = ST_GREEN_NEON;
