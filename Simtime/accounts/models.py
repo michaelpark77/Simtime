d672a26d97bf5045732c994215b66ece8795bd3e
arara90 <ararajo@gmail.com>
2020-06-23 18:13:32 +0900

    modeling

--- Simtime/accounts/models.py
+++ Simtime/accounts/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class Gender(models.TextChoices):
-    Male = 'Male'
-    Female = 'Female'
-    Custom = "Custom"
+    Male = 'M'
+    Female = 'F'
+    Custom = "C"  # Custom
 
 # Create your models here.
+
+
 class Account(AbstractUser):
-        date_of_birth = models.DateField(null=True)
-        gender = models.CharField(max_length=6, choices=Gender.choices, default=Gender.Custom)
+    date_of_birth = models.DateField(null=True)
+    gender = models.CharField(
+        max_length=6, choices=Gender.choices, default=Gender.Custom)
